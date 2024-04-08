8c44771ff0c0d9f7a2caf159a222a7aa5ee23bd7
AMANDA POMPONET DOS SANTOS <021.898677@senaicimatec.edu.br>
2024-04-08 17:59:59 -0300

    segunda versao

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react';
+import Navbar from './Navbar.js'; // Importe o componente Navbar
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar /> 
+    </div>
+  );
+}
+
+export default App;
